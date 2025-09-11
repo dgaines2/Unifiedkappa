111c3eaa2aba3a22369c0682f6e04ee1a8e7f002
Dale Gaines <dalegainesii@gmail.com>
2025-09-11 13:37:56 -0700

    Move symprec to class init

--- unifiedkappa/unified_kappa.py
+++ unifiedkappa/unified_kappa.py
@@ -32,6 +32,7 @@ class UnifiedkappaManager:
         self,
         phonon,
         mesh=25.0,
+        symprec=1e-03,
         shengbte_dir=None,
         n_histogram_bins=50,
         save_histogram=False,
@@ -40,6 +41,10 @@ class UnifiedkappaManager:
         Args:
             phonon (phonopy.Phonopy)
             mesh (float | np.array([3,], dtype=int)): q-point mesh or mesh density
+            symprec (float | None): if not None, symprec for determining the symmetry
+                of the crystal structure. This will be used to symmetrize the final
+                tensors to be invariant with respect to the symmetry operations of
+                the crystal structure
             shengbte_dir (str | Path): directory for ShengBTE outputs
             n_histogram_bins (int): if save_histogram is True, the number of
                 bins for diagonal and off diagonal components of unifiedkappa
@@ -50,6 +55,7 @@ class UnifiedkappaManager:
         self.mesh = (
             np.asarray(mesh, dtype=int) if isinstance(mesh, np.ndarray) else mesh
         )
+        self.symprec = symprec
         self.shengbte_dir = shengbte_dir
         self.n_histogram_bins = n_histogram_bins
         self.save_histogram = save_histogram
@@ -113,7 +119,6 @@ class UnifiedkappaManager:
         gvfull,
         temperature=300.0,
         freqcf=0.1,
-        symprec=1e-03,
         filename_prefix=None,
     ):
         """
@@ -126,10 +131,6 @@ class UnifiedkappaManager:
             temperature (float): temperature in Kelvin
             freqcf (float): cutoff frequency. Any frequency below this value will not
                 contribute to the thermal conductivity
-            symprec (float): if not None, symprec for determining the symmetry
-                of the crystal structure. This will be used to symmetrize the final
-                tensors to be invariant with respect to the symmetry operations of
-                the crystal structure
             filename_prefix (str): beginning of filename for saving outputs
         """
         # Units
@@ -207,10 +208,10 @@ class UnifiedkappaManager:
         kappaD = kappaD.real
         kappaOD = kappaOD.real
         kappaF = kappaF.real
-        if symprec is not None:
+        if self.symprec is not None:
             ph_symm = Symmetry(
                 self.phonon.primitive,
-                symprec=symprec,
+                symprec=self.symprec,
                 is_symmetry=True,
             )
             symmetrize_2nd_rank_tensor = partial(
@@ -221,6 +222,7 @@ class UnifiedkappaManager:
             kappaD = symmetrize_2nd_rank_tensor(kappaD)
             kappaOD = symmetrize_2nd_rank_tensor(kappaOD)
             kappaF = symmetrize_2nd_rank_tensor(kappaF)
+
         return kappaD, kappaOD, kappaF
 
     def run_minikappa(
